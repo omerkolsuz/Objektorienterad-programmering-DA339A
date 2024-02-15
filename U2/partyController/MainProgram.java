0aaa3efa898591b39b9cb74f6aa1dbbd47d0aec9
Ömer Kolsuz <112700914+omerkolsuz@users.noreply.github.com>
2024-02-15 12:34:05 +0100

    Add files via upload

---
+++ U2/partyController/MainProgram.java
@@ -0,0 +1,23 @@
+/*
+  Author: <Ömer Kolsuz>
+  Id: <an3894>
+  Study program: <DT>
+*/
+package partyController;
+
+import javax.swing.*;
+
+public class MainProgram
+{
+    public static void main(String[] args)
+    {
+        int maxNbrOfGuests = 10; // Change this line later. Only using 10 as a default value to make compilations possible.
+        /* Write code to read max number of guests from the user by using one of
+         - JOptionPane
+         - Scanner and prompt
+        */
+        String numberOfGuests = JOptionPane.showInputDialog("How Many guests are expected");
+        maxNbrOfGuests = Integer.parseInt(numberOfGuests);
+        Controller controller = new Controller(maxNbrOfGuests);
+    }
+}
